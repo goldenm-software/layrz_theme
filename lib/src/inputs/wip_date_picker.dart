c7406920b2d96961522be9bab412a91ce030f543
Manuel Romero <mra1796@gmail.com>
2023-08-18 01:00:25 -0400

    WIP

--- lib/src/inputs/wip_date_picker.dart
+++ lib/src/inputs/wip_date_picker.dart
@@ -31,7 +31,7 @@ class ThemedWipDatePicker extends StatefulWidget {
     this.showTime = false,
     this.padding = const EdgeInsets.all(10),
     this.heightFactor = 0.7,
-    this.maxHeight = 485,
+    this.maxHeight = 300,
     this.forceMobile = false,
     this.locale,
   }) : assert(label == null || labelText == null);
@@ -50,11 +50,14 @@ class _ThemedWipDatePickerState extends State<ThemedWipDatePicker> with TickerPr
   bool get forceMobile => widget.forceMobile;
   String dateStr = '';
   DateTime focusDay = DateTime.now();
-  late List<DateTime> daysInMonth = _getDaysInMonth(focusDay);
-  late List<String> daysOfWeek = DateFormat.EEEE(widget.locale?.toString()).dateSymbols.STANDALONEWEEKDAYS;
-  late List allDays = [...daysOfWeek, ...daysInMonth];
-
+  List<DateTime> daysInMonth = [];
+  List<String> daysOfWeek = [];
+  List allDays = [];
   late Function(void Function()) overlaySetState;
+  List<DateTime> initialAndFinalDay = [];
+  List<DateTime> datesSelected = [];
+  ThemeData get theme => Theme.of(context);
+  Size get size => MediaQuery.of(context).size;
 
   @override
   void initState() {
@@ -82,6 +85,14 @@ class _ThemedWipDatePickerState extends State<ThemedWipDatePicker> with TickerPr
     );
   }
 
+  _updateDate() {
+    daysInMonth = _getDaysInMonth(focusDay);
+    daysOfWeek = size.width <= kSmallGrid
+        ? DateFormat.EEEE(widget.locale?.toString()).dateSymbols.SHORTWEEKDAYS
+        : DateFormat.EEEE(widget.locale?.toString()).dateSymbols.STANDALONEWEEKDAYS;
+    allDays = [...daysOfWeek, ...daysInMonth];
+  }
+
   void _handleTap() {
     if (overlayEntry == null) {
       _updateDate();
@@ -89,12 +100,6 @@ class _ThemedWipDatePickerState extends State<ThemedWipDatePicker> with TickerPr
     } else {
       _destroyOverlay();
     }
-  }
-
-  _updateDate() {
-    daysInMonth = _getDaysInMonth(focusDay);
-    daysOfWeek = DateFormat.EEEE(widget.locale?.toString()).dateSymbols.STANDALONEWEEKDAYS;
-    allDays = [...daysOfWeek, ...daysInMonth];
   }
 
   void _buildOverlay() {
@@ -141,93 +146,17 @@ class _ThemedWipDatePickerState extends State<ThemedWipDatePicker> with TickerPr
                       },
                       child: Container(
                         padding: const EdgeInsets.all(10),
+                        height: widget.maxHeight,
                         decoration: BoxDecoration(
-                          color: Theme.of(context).cardColor,
+                          color: theme.cardColor,
                           borderRadius: BorderRadius.circular(10),
                         ),
-                        height: widget.maxHeight,
                         child: Column(
                           children: [
-                            // Row(
-                            //   children: [
-                            //     Expanded(
-                            //       child: Text(
-                            //         DateFormat('MMMM').format(focusDay),
-                            //         style: Theme.of(context).textTheme.bodyLarge?.copyWith(
-                            //               color: validateColor(color: Theme.of(context).cardColor),
-                            //               fontWeight: FontWeight.bold,
-                            //             ),
-                            //       ),
-                            //     ),
-                            //     const SizedBox(width: 10),
-                            //     const SizedBox(height: 20, child: VerticalDivider()),
-                            //     const SizedBox(width: 10),
-                            //     Tooltip(
-                            //       message: "Chao",
-                            //       child: InkWell(
-                            //         child: Icon(
-                            //           MdiIcons.calendar,
-                            //           color: validateColor(color: Theme.of(context).cardColor),
-                            //         ),
-                            //         onTap: () {
-                            //           setState(() {
-                            //             focusDay = DateTime.now();
-                            //             _updateDate();
-                            //           });
-                            //         },
-                            //       ),
-                            //     ),
-                            //     const SizedBox(width: 10),
-                            //     const SizedBox(height: 20, child: VerticalDivider()),
-                            //     const SizedBox(width: 10),
-                            //     ThemedButton(
-                            //       labelText: "hola",
-                            //       icon: MdiIcons.chevronLeft,
-                            //       style: ThemedButtonStyle.fab,
-                            //       onTap: () {
-                            //         setState(() {
-                            //           focusDay = focusDay.subtract(Duration(days: focusDay.day + 1));
-                            //           _updateDate();
-                            //         });
-                            //       },
-                            //     ),
-                            //     ThemedButton(
-                            //       labelText: "chao",
-                            //       icon: MdiIcons.chevronRight,
-                            //       style: ThemedButtonStyle.fab,
-                            //       onTap: () {
-                            //         setState(() {
-                            //           DateTime newSearch = focusDay.add(Duration(days: focusDay.day + 1));
-                            //           if (newSearch.month == focusDay.month) {
-                            //             focusDay = newSearch.add(const Duration(days: 2));
-                            //           } else {
-                            //             focusDay = newSearch;
-                            //           }
-                            //           _updateDate();
-                            //         });
-                            //       },
-                            //     ),
-                            //   ],
-                            // ),
                             _buildToolBar(),
                             Expanded(
                               child: _buildCalendar(items: allDays),
                             ),
-                            // Expanded(
-                            //   child: GridView.builder(
-                            //     gridDelegate: const ThemedGridDelegateWithFixedHeight(
-                            //       crossAxisCount: 7,
-                            //       height: 40,
-                            //     ),
-                            //     itemCount: allDays.length,
-                            //     shrinkWrap: true,
-                            //     itemBuilder: (context, index) {
-                            //       final day = allDays[index];
-                            //       debugPrint('an item was created $day');
-                            //       return _buildDay(day: day, isString: day is String);
-                            //     },
-                            //   ),
-                            // ),
                           ],
                         ),
                       ),
@@ -256,11 +185,11 @@ class _ThemedWipDatePickerState extends State<ThemedWipDatePicker> with TickerPr
       children: [
         Expanded(
           child: Text(
-            DateFormat('MMMM').format(focusDay),
-            style: Theme.of(context).textTheme.bodyLarge?.copyWith(
-                  color: validateColor(color: Theme.of(context).cardColor),
-                  fontWeight: FontWeight.bold,
-                ),
+            "${DateFormat('MMMM').format(focusDay)} ${focusDay.year}",
+            style: theme.textTheme.bodyLarge?.copyWith(
+              color: validateColor(color: theme.cardColor),
+              fontWeight: FontWeight.bold,
+            ),
           ),
         ),
         const SizedBox(width: 10),
@@ -271,7 +200,7 @@ class _ThemedWipDatePickerState extends State<ThemedWipDatePicker> with TickerPr
           child: InkWell(
             child: Icon(
               MdiIcons.calendar,
-              color: validateColor(color: Theme.of(context).cardColor),
+              color: validateColor(color: theme.cardColor),
             ),
             onTap: () {
               overlaySetState(() {
@@ -284,61 +213,39 @@ class _ThemedWipDatePickerState extends State<ThemedWipDatePicker> with TickerPr
         const SizedBox(width: 10),
         const SizedBox(height: 20, child: VerticalDivider()),
         const SizedBox(width: 10),
-        // ThemedButton(
-        //   labelText: "Previous",
-        //   icon: MdiIcons.chevronLeft,
-        //   style: ThemedButtonStyle.fab,
-        //   onTap: () {
-        //     overlaySetState(() {
-        //       focusDay = focusDay.subtract(Duration(days: focusDay.day + 1));
-        //       _updateDate();
-        //     });
-        //   },
-        // ),
-        Tooltip(
-          message: "Previous",
-          child: InkWell(
-            child: Icon(
-              MdiIcons.chevronLeft,
-              color: validateColor(color: Theme.of(context).cardColor),
-            ),
-            onTap: () {
-              overlaySetState(() {
-                focusDay = focusDay.subtract(Duration(days: focusDay.day + 1));
-                _updateDate();
-              });
-            },
-          ),
+        ThemedButton(
+          labelText: "Previous",
+          icon: MdiIcons.chevronLeft,
+          style: ThemedButtonStyle.fab,
+          onTap: () {
+            overlaySetState(() {
+              focusDay = focusDay.subtract(Duration(days: focusDay.day + 1));
+              debugPrint("Dates Selected: $datesSelected");
+              _updateDate();
+            });
+          },
         ),
-
-        Builder(builder: (context) {
-          return ThemedButton(
-            labelText: "chao",
-            icon: MdiIcons.chevronRight,
-            style: ThemedButtonStyle.fab,
-            onTap: () {
-              // setState1;
-              overlaySetState(() {
-                DateTime newSearch = focusDay.add(Duration(days: focusDay.day + 1));
-                if (newSearch.month == focusDay.month) {
-                  focusDay = newSearch.add(const Duration(days: 2));
-                } else {
-                  focusDay = newSearch;
-                }
-                _updateDate();
-              });
-            },
-          );
-        }),
+        ThemedButton(
+          labelText: "Previous",
+          icon: MdiIcons.chevronRight,
+          style: ThemedButtonStyle.fab,
+          onTap: () {
+            overlaySetState(() {
+              DateTime newSearch = focusDay.add(Duration(days: focusDay.day + 1));
+              if (newSearch.month == focusDay.month) {
+                focusDay = newSearch.add(const Duration(days: 2));
+              } else {
+                focusDay = newSearch;
+              }
+              _updateDate();
+            });
+          },
+        ),
       ],
     );
   }
 
   Widget _buildCalendar({required List items}) {
-    // final List<DateTime> daysInMonth = _getDaysInMonth(focusDay);
-    // final List<String> daysOfWeek = DateFormat.EEEE(widget.locale?.toString()).dateSymbols.STANDALONEWEEKDAYS;
-    // final List allDays = [...daysOfWeek, ...daysInMonth];
-
     return GridView.builder(
       gridDelegate: const ThemedGridDelegateWithFixedHeight(
         crossAxisCount: 7,
@@ -348,52 +255,105 @@ class _ThemedWipDatePickerState extends State<ThemedWipDatePicker> with TickerPr
       shrinkWrap: true,
       itemBuilder: (context, index) {
         final day = items[index];
-        debugPrint('an item was created $day');
         return _buildDay(day: day, isString: day is String);
       },
     );
   }
 
-  // List<DateTime> _getDaysInMonth(DateTime month) {
-  //   final daysInMonth = DateTime(month.year, month.month + 1, 0).day;
-  //   return List.generate(daysInMonth, (index) => DateTime(month.year, month.month, index + 1));
-  // }
   List<DateTime> _getDaysInMonth(DateTime month) {
-    final daysInMonth = DateTime.utc(month.year, month.month + 1, 0).day;
-    return List<DateTime?>.filled(daysInMonth, null, growable: false)
-        .asMap()
-        .map((index, value) => MapEntry(index, DateTime.utc(month.year, month.month, index + 1)))
-        .values
-        .toList();
+    final daysInMonth = DateTime(month.year, month.month + 1, 0).day;
+    return List.generate(daysInMonth, (index) => DateTime(month.year, month.month, index + 1));
   }
 
   Widget _buildDay({required var day, required bool isString}) {
     return isString
         ? Center(child: Text(day))
-        : Center(
-            child: Material(
-              color: Colors.transparent,
-              child: InkWell(
-                onTap: () {
-                  debugPrint("daysOfWeek: $day - ${day.runtimeType}");
-                },
-                borderRadius: BorderRadius.circular(50),
-                child: Container(
-                  width: 30,
-                  height: 30,
-                  padding: const EdgeInsets.all(5),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(50),
-                    color: Colors.red,
-                  ),
-                  child: Center(
-                    child: Text(
-                      day.day.toString(),
+        : Builder(builder: (dayContext) {
+            Color textContainer = _getTextContainerColor(day);
+            Color textColor = _getLabelTextColor(day);
+            return Center(
+              child: Material(
+                color: Colors.transparent,
+                child: InkWell(
+                  onTap: () {
+                    debugPrint("daysOfWeek: $day - ${day.runtimeType}");
+                    _selectInitialAndFinalDay(day);
+                    datesSelected = _setDateSelected();
+                    overlaySetState(() {});
+                    debugPrint("initialAndFinalDay: $initialAndFinalDay");
+                  },
+                  borderRadius: BorderRadius.circular(50),
+                  child: Container(
+                    width: 30,
+                    height: 30,
+                    padding: const EdgeInsets.all(5),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(50),
+                      color: textContainer,
+                    ),
+                    child: Center(
+                      child: Text(
+                        day.day.toString(),
+                        style: theme.textTheme.bodyMedium?.copyWith(
+                          color: textColor,
+                        ),
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-          );
+            );
+          });
+  }
+
+  Color _getTextContainerColor(DateTime day) {
+    if (initialAndFinalDay.contains(day)) {
+      return theme.primaryColor;
+    } else {
+      if (datesSelected.contains(day)) {
+        return theme.primaryColor.withOpacity(0.2);
+      }
+    }
+    return theme.cardColor;
+  }
+
+  Color _getLabelTextColor(DateTime day) {
+    if (initialAndFinalDay.contains(day)) {
+      return Colors.white;
+    } else {
+      if (datesSelected.contains(day)) {
+        return theme.primaryColor;
+      }
+    }
+
+    return Colors.black;
+  }
+
+  void _selectInitialAndFinalDay(DateTime day) {
+    if (initialAndFinalDay.isEmpty) {
+      initialAndFinalDay.add(day);
+    } else if (initialAndFinalDay.length == 1) {
+      initialAndFinalDay.add(day);
+      if (initialAndFinalDay.first.isAfter(initialAndFinalDay.last)) {
+        initialAndFinalDay = initialAndFinalDay.reversed.toList();
+      }
+    } else {
+      initialAndFinalDay = [day];
+    }
+    debugPrint("initialAndFinalDay: $initialAndFinalDay");
+  }
+
+  List<DateTime> _setDateSelected() {
+    List<DateTime> list = [];
+    if (initialAndFinalDay.isNotEmpty) {
+      DateTime initialDay = initialAndFinalDay.first;
+      DateTime finalDay = initialAndFinalDay.last;
+      // find all days between initial and final day
+      for (int i = 0; i <= finalDay.difference(initialDay).inDays; i++) {
+        list.add(initialDay.add(Duration(days: i)));
+      }
+    }
+    debugPrint("list: $list");
+    return list;
   }
 }
