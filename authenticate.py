9a2efd80a72a5c2ae24157cbe1a3435a92bd72dc
Kenny Mochizuki <kenny.mochizuki@gmail.com>
2023-11-06 17:25:54 -0600

    New attempt using full python to authenticate

--- authenticate.py
+++ authenticate.py
@@ -12,4 +12,10 @@ result = os.path.join(current_dir, 'credentials.json')
 with open(result, 'w', encoding='utf-8') as f:
     f.write(json.dumps(data))
 
-print(result)
+os.system('gcloud auth activate-service-account --key-file=credentials.json')
+os.system(
+    'gcloud auth print-identity-token --audiences=https://pub.dev | ' +\
+    'dart pub token add https://pub.dev'
+)
+
+os.remove('credentials.json')
