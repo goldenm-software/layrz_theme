23d87172a002f5e4d5b31dc6510244a22d8a8c2a
Luis Rogelio Reyes <luisrogelio98@gmail.com>
2023-12-01 19:17:09 -0400

    4.3.4 ThemedTable fix

--- lib/src/inputs/src/general/duallist_input.dart
+++ lib/src/inputs/src/general/duallist_input.dart
@@ -1,4 +1,4 @@
-part of inputs;
+part of '../../inputs.dart';
 
 class ThemedDualListInput<T> extends StatefulWidget {
   /// [labelText] is the label text of the dual list input. Avoid submit [label] and [labelText] at the same time.
