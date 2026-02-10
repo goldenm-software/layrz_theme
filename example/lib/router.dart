c9fe01252020c2152fc308d19e4295ef2b1799c8
Kenny Mochizuki <kenny@goldenm.com>
2026-02-10 17:18:47 -0600

    Fix

--- example/lib/router.dart
+++ example/lib/router.dart
@@ -145,7 +145,7 @@ final goRoutes = [
 ];
 
 final router = GoRouter(
-  initialLocation: kDebugMode ? '/inputs/chips' : '/',
+  initialLocation: kDebugMode ? '/map/layer' : '/',
   errorPageBuilder: (context, state) => customTransitionBuilder(context, state, const NotFoundView()),
   routes: goRoutes,
 );
